45d67a6520008d725e4148331ee3920f9e89c256
Ulisses <94124421+odysseushype@users.noreply.github.com>
2025-09-01 08:51:13 -0300

    Update relatorio.py

--- relatorios/relatorio.py
+++ relatorios/relatorio.py
@@ -216,6 +216,7 @@ if not df.empty:
         
         # Processar CA05
        # Processar CA05 
+        qtd_aprovada = pd.to_numeric(row["Qtd Aprovada"], errors="coerce")
         if not registros_ca05_sem_roteiro.empty: # Agrupar por Descrição Item para encontrar a primeira Qtd Aprovada de cada item 
             itens_unicos = {} 
             for _, row in registros_ca05_sem_roteiro.iterrows(): 
@@ -223,7 +224,7 @@ if not df.empty:
                 if item not in itens_unicos and not pd.isna(row["Qtd Aprovada"]): 
                     itens_unicos[item] = { 
                         "qtd": row["Qtd Aprovada"], 
-                        "roteiro": "RAPIDO" if row["Qtd Aprovada"] <= 18000 else "LENTO", 
+                        "roteiro": "RAPIDO" if qtd_aprovada <= 18000 else "LENTO", 
                         "velocidade": 50000 if row["Qtd Aprovada"] <= 18000 else 70000
                     }
             # Atribuir roteiros e atualizar o DataFrame
@@ -1154,4 +1155,5 @@ with tab2:
     else:
 
         st.info("Nenhum dado disponível para gráficos detalhados.")
+
 
